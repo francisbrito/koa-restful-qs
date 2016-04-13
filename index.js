707e6d63054fbf516daebe782d5a8aa68d554c5c
Francis Brito <francis@instacarro.com>
2016-04-13 19:27:01 -0400

    Update factory so it parses `context.query`.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
+const parseRESTfulQuery = require('restful-qs');
 
-function restfulQueryParser() {
-  return function* parseRESTfulQuery() {};
+function restfulQueryParser(options) {
+  options = options || {};
+
+  const parse = options.parse || parseRESTfulQuery;
+
+  return function* parseRESTfulQueryMiddleware(next) {
+    this.state.restfulQuery = parse(this.request.query);
+    yield next;
+  };
 }
 
 module.exports = restfulQueryParser;
--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 'use strict';
+const spy = require('spy');
 const test = require('tape');
 
 const restfulQueryParser = require('./');
@@ -7,6 +8,28 @@ test('it returns a generator function.', t => {
   const subject = restfulQueryParser();
 
   t.ok(isGeneratorFn(subject), 'it should be a generator function.');
+
+  t.end();
+});
+
+test('it calls RESTful query parser with `context.query`', t => {
+  const expectedQuery = {
+    age: '29',
+    name: 'sabrina',
+  };
+  const context = {
+    state: {},
+    request: {
+      query: expectedQuery,
+    },
+  };
+  const parse = spy();
+  const subject = restfulQueryParser({ parse }).call(context);
+
+  subject.next();
+
+  t.ok(parse.called, 'should have called `parse`.');
+  t.ok(parse.calledWith(expectedQuery), 'should have called `parse` with expected query.');
 
   t.end();
 });
